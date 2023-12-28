029860e7b87c39e83cea0fc7525bd28bc357bfd1
maria <mariadsn89@gmail.com>
2023-12-28 12:18:18 +0100

    background update

--- enrichmentAnalysis.R
+++ enrichmentAnalysis.R
@@ -1,10 +1,10 @@
 # Analisis de enriquecimiento funcional
 BiocManager::install("clusterProfiler")
-BiocManager::install("org.Hs.eg.db")
+BiocManager::install("hgu133plus2.db")
 BiocManager::install("DO.db")
 
 library(BiocManager)
-library(org.Hs.eg.db)
+library(hgu133plus2.db)
 library(clusterProfiler)
 library(DO.db)
 library(ggplot2)
@@ -17,8 +17,8 @@ archivos_txt <- archivos[grep("\\.txt$", archivos)]
 # Analisis para GO
 for (archivo in archivos_txt) {
   genelist <- readLines(paste0(carpeta,archivo))
-  entrez_ids <- mapIds(org.Hs.eg.db, keys=genelist, column='ENTREZID', keytype='SYMBOL')
-  go <- enrichGO(entrez_ids,  OrgDb = "org.Hs.eg.db", ont = "all", pAdjustMethod = "fdr")
+  entrez_ids <- mapIds(hgu133plus2.db, keys=genelist, column='ENTREZID', keytype='SYMBOL')
+  go <- enrichGO(entrez_ids,  OrgDb = "hgu133plus2.db", ont = "all", pAdjustMethod = "fdr")
   if(length(go@result$ONTOLOGY) != 0){
     write.csv(go, paste0(carpeta,archivo,"all_BH.csv"), row.names = FALSE)
     grafico <- dotplot(go, split="ONTOLOGY") + facet_grid(ONTOLOGY~., scale="free")
@@ -29,7 +29,7 @@ for (archivo in archivos_txt) {
 # Analisis para KEGG
 for (archivo in archivos_txt) {
   genelist <- readLines(paste0(carpeta,archivo))
-  entrez_ids <- mapIds(org.Hs.eg.db, keys=genelist, column='ENTREZID', keytype='SYMBOL')
+  entrez_ids <- mapIds(hgu133plus2.db, keys=genelist, column='ENTREZID', keytype='SYMBOL')
   kegg <- enrichKEGG(entrez_ids, pAdjustMethod = "fdr")
   if(!is.null(kegg)){
     if(any(kegg@result$p.adjust <0.05)){
